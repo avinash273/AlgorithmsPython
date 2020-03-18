b2369f62921d3d8f7b499edc210ec6a050fdf496
avinash273 <avinash.shanker@mavs.uta.edu>
2020-03-17 23:40:13 -0500

    classes

--- Ch2/classes_start.py
+++ Ch2/classes_start.py
@@ -1,10 +1,17 @@
 #
 # Example file for working with classes
 #
+class myClass():
+  def method1(self):
+    print("myClass Method1")
 
+  def method2(self,someString):
+    print("myClass Method1 " + someString )
 
 def main():
-
+  c = myClass()
+  c.method1()
+  c.method2("String obj")
   
 if __name__ == "__main__":
   main()
