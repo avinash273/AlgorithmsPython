32b014baeef71f95dbb25221d0faac957721d60e
avinash273 <avinash.shanker@mavs.uta.edu>
2020-03-17 22:13:41 -0500

    Traditional welcome

--- Ch2/variables_start.py
+++ Ch2/variables_start.py
@@ -1,15 +1,25 @@
 # 
 # Example file for variables
 #
-
+f = 0
+print(f)
 # Declare a variable and initialize it
-
+f = 'abc'
+print(f)
 
 # # re-declaring the variable works
-
+print("this is string " + f + str(123))
 
 # # ERROR: variables of different types cannot be combined
 
 
 # Global vs. local variables in functions
+def someFunction():
+    global f
+    f = 'def'
+    print(f)
 
+someFunction()
+print(f)
+del(f)
+print(f)
