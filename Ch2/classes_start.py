78c0bc7d4d8815538be0f6df44f51937f6e33dff
avinash273 <avinash.shanker@mavs.uta.edu>
2020-03-17 23:45:37 -0500

    Multi inheritance

--- Ch2/classes_start.py
+++ Ch2/classes_start.py
@@ -6,12 +6,25 @@ class myClass():
     print("myClass Method1")
 
   def method2(self,someString):
-    print("myClass Method1 " + someString )
+    print("myClass Method2 " + someString )
+
+class anotherClass(myClass):
+  def method1(self):
+    myClass.method1(self)
+    print("anotherClass Method1")
+
+  def method2(self,someString):
+    myClass.method2(self,"Dummy value")
+    print("anotherClass Method2 " + someString )
 
 def main():
   c = myClass()
   c.method1()
   c.method2("String obj")
+
+  c2 = anotherClass()
+  c2.method1()
+  c2.method2("Inherited method class2")
   
 if __name__ == "__main__":
   main()
