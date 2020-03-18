80e1fc0b44de9d32355eabf9f1c67ab5e01ba68a
avinash273 <avinash.shanker@mavs.uta.edu>
2020-03-18 00:43:31 -0500

    datetime

--- Ch3/dates_start.py
+++ Ch3/dates_start.py
@@ -10,21 +10,21 @@ def main():
   # Get today's date from the simple today() method from the date class
   today = date.today()
   timenow = datetime.now()
-  print("Date is: ", today)
+  #print("Date is: ", today)
   Month = ["Jan", "Feb", "Mar", "Apr"]
 
   # print out the date's individual components
   days = ["mon", "tue", "wed", "thu", "fri" "sat", "sun"]
-  print("Date Components: ", days[today.weekday()])
+  #print("Date Components: ", days[today.weekday()])
 
 
   # retrieve today's weekday (0=Monday, 6=Sunday)
   days = ["Mon", "Tue", "Wed", "Thu", "Fri" "Sat", "Sun"]
-  print("Date Components: ", days[today.weekday()])
+  #print("Date Components: ", days[today.weekday()])
   
   ## DATETIME OBJECTS
   # Get today's date from the datetime class
-  print(days[today.weekday()],"",Month[today.month-1]," ",timenow)
+  print(days[today.weekday()],"",Month[today.month-1],"",timenow.strftime("%I:%M:%S %p"))
 
   # Get the current time
 
