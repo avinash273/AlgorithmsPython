76084015188de59213d7d6b2f1d7a45aeef1d4c5
avinash273 <avinash.shanker@mavs.uta.edu>
2020-03-17 22:52:40 -0500

    conditionals

--- Ch2/conditionals_start.py
+++ Ch2/conditionals_start.py
@@ -3,12 +3,21 @@
 #
 
 def main():
-  x, y = 10, 100
+  x, y = 10, 10
   
-  # conditional flow uses if, elif, else  
+  # conditional flow uses if, elif, else
+  if (x > y):
+    st = "x > y"
+  elif ( x == y):
+    st = "x == y"
+  else:
+    st = "y < x"
+
+  print(st)
 
   # conditional statements let you use "a if C else b"
-  
+  st = "x > y" if (x > y) else "x < = y"
+  print(st)
 
 if __name__ == "__main__":
   main()
