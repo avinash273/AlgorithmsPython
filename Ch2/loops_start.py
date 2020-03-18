c356344f2a78438774669b52bb7791969bd00a9c
avinash273 <avinash.shanker@mavs.uta.edu>
2020-03-17 23:30:26 -0500

    breaks

--- Ch2/loops_start.py
+++ Ch2/loops_start.py
@@ -6,16 +6,24 @@ def main():
   x = 0
 
   # define a while loop
-
+  while (x < 5):
+    print("While: ",x)
+    x = x+1
 
   # define a for loop
-
+  for x in range(5,10):
+    print("For: ",x)
 
   # use a for loop over a collection
-    
+  days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+
+  for x in days:
+    print("All Days: ",x)
  
   # use the break and continue statements
-
+  for x in range(5,10):
+    if(x==7): break
+    print(x)
 
   #using the enumerate() function to get index 
 
