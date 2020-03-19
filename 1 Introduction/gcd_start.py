8cb3f276bb7e6527a3ea62b2db4bb8422f8b20ad
avinash273 <avinash.shanker@mavs.uta.edu>
2020-03-18 19:40:59 -0500

    GCD, While and Recursive way.

--- 1 Introduction/gcd_start.py
+++ 1 Introduction/gcd_start.py
@@ -2,10 +2,24 @@
 # using Euclid's algorithm
 
 
+def gcdRecursive(a, b):
+    if(b == 0):
+        return a
+    return gcd(b, a%b)
+
 def gcd(a, b):
-    pass
-    
+    while b != 0:
+        t = a
+        a = b
+        b = t % b
+    return a
+
+
+
         
 # try out the function with a few examples
-print(gcd(60, 96))  # should be 12
-print(gcd(20, 8))   # should be 4
+print("While ", gcd(60, 96))  # should be 12
+print("While ",gcd(20, 8))   # should be 4
+
+print("Recursive ",gcdRecursive(60, 96))  # should be 12
+print("Recursive ",gcdRecursive(20, 8))   # should be 4
