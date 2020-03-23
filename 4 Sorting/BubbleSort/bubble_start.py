0b0f6d0e2ae70769f2b352f71bf4b1b5dc9ba5ae
avinash273 <avinash.shanker@mavs.uta.edu>
2020-03-22 22:49:45 -0500

    Bubble sort un optimized

--- 4 Sorting/BubbleSort/bubble_start.py
+++ 4 Sorting/BubbleSort/bubble_start.py
@@ -4,7 +4,7 @@
 def bubbleSort(dataset):
     # TODO: start with the array length and decrement each time
     for i in range(len(dataset)):
-        for j in range(len(dataset)):
+        for j in range(i):
             if(dataset[i] < dataset[j]):
                 temp = dataset[i]
                 dataset[i] = dataset[j]
@@ -12,11 +12,10 @@ def bubbleSort(dataset):
         print("Current state: ", dataset)
     return dataset
 
-
 def main():
     list1 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
     bubbleSort(list1)
-    print("Result: ", list1)
+    print("\nResult: ", list1)
 
 
 if __name__ == "__main__":
