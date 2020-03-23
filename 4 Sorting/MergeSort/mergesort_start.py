e0e24866d4f7c25121b8db0ba46efee69750a7c9
avinash273 <avinash.shanker@mavs.uta.edu>
2020-03-23 00:02:29 -0500

    merge sort from scratch

--- 4 Sorting/MergeSort/mergesort_start.py
+++ 4 Sorting/MergeSort/mergesort_start.py
@@ -1,33 +1,42 @@
-# Implement a merge sort with recursion
-
+# TODO merge sort from the start
 
 items = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
 
 def mergesort(dataset):
     if len(dataset) > 1:
-        mid = len(dataset) // 2
+        mid = len(dataset)//2
         leftarr = dataset[:mid]
         rightarr = dataset[mid:]
+        mergesort(leftarr)
+        mergesort(rightarr)
+        i = 0 # index into the left array
+        j = 0 #index into the right array
+        k = 0 #index into merged array
 
-        # TODO: recursively break down the arrays
+        # TODO while both arrays have content
+        while i < len(leftarr) and j < len(rightarr):
+            if leftarr[i] < rightarr[j]:
+                dataset[k] = leftarr[i]
+                i += 1
+            else:
+                dataset[k] = rightarr[j]
+                j += 1
+            k += 1
+
+        # TODO: If the left array still has values, add them
+        while i < len(leftarr):
+            dataset[k] = leftarr[i]
+            i += 1
+            k += 1
 
 
-        # TODO: now perform the merging
-        i=0 # index into the left array
-        j=0 # index into the right array
-        k=0 # index into merged array
+        #TODO: if right array still has values, add them
+        while j < len(rightarr):
+            dataset[k] = rightarr[j]
+            j += 1
+            k += 1
+    return dataset
 
-        # TODO: while both arrays have content
-
-
-        # TODO: if the left array still has values, add them
-
-
-        # TODO: if the right array still has values, add them
-
-
-
-# test the merge sort with data
 print(items)
-mergesort(items)
-print(items)
+
+print(mergesort(items))
