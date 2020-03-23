ce26fdaf5e898d04368bd4541d02831841485d7d
avinash273 <avinash.shanker@mavs.uta.edu>
2020-03-22 21:10:24 -0500

    queue pop popleft deque

--- 2 Data Structures/Queues/queue_start.py
+++ 2 Data Structures/Queues/queue_start.py
@@ -1,12 +1,23 @@
 # try out the Python queue functions
-
+from collections import deque
 
 # TODO: create a new empty deque object that will function as a queue
-stacks = []
+queue = deque()
 
 # TODO: add some items to the queue
+queue.append(1)
+queue.append(2)
+queue.append(3)
+queue.append(4)
+queue.append(5)
 
 # TODO: print the queue contents
-print(stacks)
+print(queue)
 
 # TODO: pop an item off the front of the queue
+x = queue.popleft()
+print(x)
+print(queue)
+x = queue.pop()
+print(x)
+print(queue)
