787d83bd395ff85964c37c49c9968b4f5f5c8144
avinash273 <avinash.shanker@mavs.uta.edu>
2020-04-06 01:46:02 -0500

    Brackets stack

--- 6 Other Algorithms/ValueCounter_start.py
+++ 6 Other Algorithms/ValueCounter_start.py
@@ -2,19 +2,28 @@
 
 
 # define a set of items that we want to count
+
+x = print(counter)
 items = ["apple", "pear", "orange", "banana", "apple",
          "orange", "apple", "pear", "banana", "orange",
          "apple", "kiwi", "pear", "apple", "orange"]
 
+paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+banned = ["hit"]
+paragraph = paragraph.lower()
+paragraph = paragraph.replace(',','')
+paragraph = paragraph.replace('.','')
+print(paragraph)
+
+words = paragraph.split()
 # TODO: create a hashtable object to hold the items and counts
 counter = dict()
 
 # TODO: iterate over each item and increment the count for each one
-for item in items:
+for item in words:
     if(item in counter.keys()):
         counter[item] += 1
     else:
         counter[item] = 1
 
 # print the results
-print(counter)
