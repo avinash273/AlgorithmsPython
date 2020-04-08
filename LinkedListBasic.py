3f70de3f066d4dd2c7d5ba10745fc320aa8230f3
avinash273 <avinash.shanker@mavs.uta.edu>
2020-04-08 17:10:38 -0500

    complete linked list

--- LinkedListBasic.py
+++ LinkedListBasic.py
@@ -62,19 +62,21 @@ class linked_list:
         if found == 0:
             print("Element not found")
 
-    def insert_node_at(self,index, data):
+    def insert_node_at(self, index, data):
         cur = self.head
         ins_node = node(data)
         count = 0
-        while cur.next is not None:
-            cur = cur.next
-            if(index == 0):
-                self.head = ins_node
-            elif index < count:
+        if index == count:
+            ins_node.next = cur.next
+            cur.next = ins_node
+        else:
+            while cur.next is not None:
+                cur = cur.next
+                cur2 = cur
                 count += 1
-            elif index == count:
-                ins_node.next = cur.next
-                cur.next = ins_node
+                if(count == index):
+                    ins_node.next = cur.next
+                    cur.next = ins_node
 
 
 
@@ -82,19 +84,26 @@ class linked_list:
 
 
 linked_list1 = linked_list()
-print("Before:", linked_list1.length())
+# print("Before:", linked_list1.length())
 linked_list1.append(1)
 linked_list1.append(2)
 linked_list1.append(3)
 linked_list1.append(4)
 linked_list1.append(5)
 
-linked_list1.display()
-print("After:", linked_list1.length())
-print("At index:", linked_list1.get(2))
+# linked_list1.display()
+# print("After:", linked_list1.length())
+# print("At index:", linked_list1.get(2))
 
 # linked_list1.erase(9)
 # linked_list1.erase(4)
 # linked_list1.display()
 # print("After erase:", linked_list1.length())
 
+# print("here")
+linked_list1.insert_node_at(0,9)
+linked_list1.insert_node_at(2,10)
+linked_list1.insert_node_at(2,10)
+linked_list1.display()
+print("after display")
+print("After erase:", linked_list1.length())
