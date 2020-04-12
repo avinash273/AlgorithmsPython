2222849d10a1b96ad698d2be279c3dbe240e01fb
avinash273 <avinash.shanker@mavs.uta.edu>
2020-04-12 15:41:59 -0500

    Queue and Deqeue insert in O(n) and pop in O(1) complexity

--- DataStructuresStacksQueuesDeques/Ch03/03_02/End/queues.py
+++ DataStructuresStacksQueuesDeques/Ch03/03_02/End/queues.py
@@ -4,16 +4,40 @@ class Queue:
         self.items = []
 
     def enqueue(self, item):
-        pass
+        """
+        Inserts an item at the zeroth index of the Queue.
+        The runtime will be O(n), because inserting into the zeroth index forces all the elements to move by one.
+        :param item:
+        :return:
+        """
+        self.items.insert(0, item)
 
     def dequeue(self):
-        pass
-        
+        """
+        :return: front most item from equeue
+        works in constant time O(1)
+        """
+        self.items.pop()
+
     def peek(self):
-        pass
+        return self.items[-1]
 
     def size(self):
-        pass
+        return len(self.items)
 
     def is_empty(self):
-        pass
+        if self.items:
+            return False
+        else:
+            return True
+
+
+myQueue = Queue()
+myQueue.enqueue('apple')
+myQueue.enqueue('banana')
+myQueue.enqueue('orange')
+print(myQueue.items)
+myQueue.dequeue()
+print(myQueue.items)
+
+print(myQueue.is_empty())
