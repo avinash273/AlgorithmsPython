161e74056e617fc20c333d8fce0cff91bb491704
avinash273 <avinash.shanker@mavs.uta.edu>
2020-04-13 02:37:20 -0500

    pre-order, in-order, post-order traversal of binary tree(corrections)

--- DetachedCoding/BinaryTreesYoutube/BinaryTrees.py
+++ DetachedCoding/BinaryTreesYoutube/BinaryTrees.py
@@ -15,7 +15,7 @@ class BinaryTree(object):
         elif traversal_type == "inorder":
             return self.inorder_print(tree.root, "")
         elif traversal_type == "postorder":
-            return self.preorder_print(tree.root, "")
+            return self.postorder_print(tree.root, "")
         else:
             print("Traversal Type unsupported")
 
@@ -54,7 +54,7 @@ tree = BinaryTree(1)
 tree.root.left = Node(2)
 tree.root.right = Node(3)
 tree.root.left.left = Node(4)
-tree.root.right.right = Node(5)
+tree.root.left.right = Node(5)
 tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
 
