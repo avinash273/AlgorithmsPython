7140828efd28a6da4f100af8906ff774d8ac1c87
avinash273 <avinash.shanker@mavs.uta.edu>
2020-04-15 20:26:30 -0500

    remove_duplicates complete

--- CrackingCodingInterview/Linked Lists/RemoveDuplicates.py
+++ CrackingCodingInterview/Linked Lists/RemoveDuplicates.py
@@ -24,12 +24,45 @@ class LinkedList:
             listData.append(cur.data)
         print(*listData, sep="->")
 
+    def remove_duplicates(self):
+        hashmap = dict()
+        cur = self.head
+        while cur.next is not None:
+            last = cur
+            cur = cur.next
 
+            if cur.data not in hashmap:
+                hashmap[cur.data] = 0
+            else:
+                last.next = cur.next
+                cur = last
+        print(hashmap)
+
+
+print("Before")
 linked_list1 = LinkedList()
 linked_list1.append(1)
+linked_list1.append(4)
+linked_list1.append(4)
+linked_list1.append(4)
+linked_list1.append(4)
+linked_list1.append(4)
+linked_list1.append(4)
 linked_list1.append(2)
 linked_list1.append(3)
 linked_list1.append(4)
+linked_list1.append(4)
+linked_list1.append(4)
 linked_list1.append(5)
+linked_list1.append(5)
+linked_list1.append(5)
+linked_list1.append(5)
+linked_list1.append(4)
+linked_list1.append(2)
+linked_list1.append(3)
 
+linked_list1.display()
+
+linked_list1.remove_duplicates()
+print("After")
 linked_list1.display()
