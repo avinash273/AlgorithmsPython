010c46fa9a26b56d339b7d4bb017a834d7b38d38
avinash273 <avinash.shanker@mavs.uta.edu>
2020-05-03 18:11:05 -0500

    GCD

--- Practice/GCD.py
+++ Practice/GCD.py
@@ -1,6 +1,17 @@
-def GCD(a, b):
+def GCD_Recursive(a, b):
     if b == 0:
         return a
-    return GCD(b, a % b)
+    return GCD_Recursive(b, a % b)
 
-print(GCD(60, 96))
+
+def GCD_Loop(a, b):
+    while b != 0:
+        temp = a
+        a = b
+        b = temp % b
+    return a
+
+
+print(GCD_Recursive(20, 8))
+print(GCD_Recursive(60, 96))
+print(GCD_Loop(20, 8))
