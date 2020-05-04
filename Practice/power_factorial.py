58536b8f2900139bd41e8a923a77edcab2b6b943
avinash273 <avinash.shanker@mavs.uta.edu>
2020-05-03 21:03:00 -0500

    factorial

---
+++ Practice/power_factorial.py
@@ -0,0 +1,16 @@
+def power(num, pwr):
+    if pwr == 0:
+        return 1
+    else:
+        return num * power(num, pwr - 1)
+
+
+def factorial(num):
+    if num == 0:
+        return 1
+    else:
+        return num * factorial(num - 1)
+
+
+print(power(2, 1))
+print(factorial(5))
